c41cd5dd26121a5598370fe1ae60fa6dc439b53a
himlayyy <alesandralelis@gmail.com>
2021-12-13 11:45:16 +0800

    Can remove single argument

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, item){
+    return arr.filter((elem) => elem != item);
+}
 
-};
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
