6a955d7d5590f75e0c4541a6d38a53c9d0435a4e
himlayyy <alesandralelis@gmail.com>
2021-12-13 09:03:25 +0800

    Completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+   let phrases = "";
+   if (num <0){
+       return "ERROR";
+   }
+   else{
+    for (i=0;i<num;i++){
+        phrases = phrases + string;
+    }
+    return phrases;
+    };
 };
 
 // Do not edit below this line
