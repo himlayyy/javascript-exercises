a95b22e988a0734b684e1c961369912d7bd27d9c
himlayyy <alesandralelis@gmail.com>
2021-12-13 14:44:14 +0800

    Completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let start = 0;
+    let stop = 0;
+    let accumulator = 0;    
+    if (a > b){
+        start = b;
+        stop = a;     
+    }
+    else if(a <= -1 || b <= -1  || typeof (a) != 'number' || typeof (b) != 'number'){
+        return "ERROR";
+    }    
+    else{
+        start = a;
+        stop = b;
+       
+    }; 
+    
+    while (start < stop+1){        
+       accumulator = accumulator + start;        
+       start++;
+    };
+   return(accumulator);
 };
 
 // Do not edit below this line
