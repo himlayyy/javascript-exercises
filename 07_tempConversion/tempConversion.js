0b3f2d0d5b4210b2f0434ae5912553e23cb9ea4d
himlayyy <alesandralelis@gmail.com>
2021-12-13 16:51:49 +0800

    Completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,8 @@
-const ftoc = function() {
+const ftoc = temp => Math.round((((temp - 32)*5)/ 9) * Math.pow(10, 1))/ Math.pow(10, 1);
 
-};
+const ctof = temp => Math.round((((temp * 9)/ 5)+32) * Math.pow(10,1)) / Math.pow(10,1);
 
-const ctof = function() {
 
-};
 
 // Do not edit below this line
 module.exports = {
