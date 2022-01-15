7650a86c032301a85337b04e37c797881b34135a
himlayyy <alesandralelis@gmail.com>
2022-01-16 01:31:00 +0800

    Completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(x,y) {
+  return x+y;	
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(numArr) {
+	return numArr.reduce((prevVal, currVal) => prevVal + currVal, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numArr) {
+  return numArr.reduce((prevVal, currVal) => prevVal * currVal, 1);
 };
 
-const power = function() {
-	
+const power = function(num, exponent) {
+  let exp = 	[...Array(exponent).keys()].map(i => num);  
+  // let finalProd = exp.reduce((prevVal, currVal) => prevVal * currVal, 1);
+  return multiply(exp);
 };
 
-const factorial = function() {
-	
+const factorial = function(val) {
+  let fracArr = [...Array(val).keys()].map(i =>i+1);
+  return multiply(fracArr);
 };
 
 // Do not edit below this line
